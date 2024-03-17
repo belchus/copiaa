8bfe5abd2aa3b4ea9b43a8b2b6a8c26eee99a058
Belen <arenabelu@gmail.com>
2024-03-17 11:36:50 -0300

    first commit

---
+++ vilmix-backend/Server/index.js
@@ -0,0 +1,57 @@
+import express  from 'express';
+import cors from 'cors';
+import { config as configDotenv } from 'dotenv'; // Cambio en la importación
+
+// Cargar las variables de entorno desde el archivo .env
+configDotenv();
+// Mercado Pago SDK
+import { MercadoPagoConfig, Preference } from 'mercadopago';
+
+// Add Your credentials
+const client = new MercadoPagoConfig({ accessToken:"TEST-6544339040733152-031522-4d7eb2feb9ef209e5b16692cae46e27b-191929307" });
+
+const server = express();
+server.use(express.json());
+server.use(cors());
+
+server.post('/create_preference',async (req,res)=>{
+   
+    try {
+        const body ={
+            items:[
+             {
+                title:req.body.nombre,
+                quantity:req.body.cantidad,
+                unit_price:req.body.precio,
+                currency_id : "ARS",
+             },
+            ],
+            back_urls:{
+                success:"https://www.mercadopago.com.ar/developers/en/docs/credentials",
+                failure:"https://www.youtube.com/watch?v=vEXwN9-tKcs&ab_channel=onthecode",
+                pending:"",
+            },
+            auto_return:"approved"
+
+        
+        };
+        const preference = new Preference(client);
+        const resutlt = await preference.create({body});
+
+ res.json({
+    id:resutlt.id,
+ });
+} catch (error) {
+    console.log(error)
+    res.status(500).json({
+        error: "ERROR AL CREAR LA PREFERENCIA"
+    })
+}
+
+})
+server.get("/",(req,res) => {
+    res.send("Servidor up")
+});
+
+server.listen(4000,() => console.log('servidor levantado') );
+
