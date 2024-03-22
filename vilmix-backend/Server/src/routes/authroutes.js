8d7ddf2d494a97b97ea885050d5f70617a2870f1
Belen <arenabelu@gmail.com>
2024-03-22 00:34:37 -0300

    se agregaron filtros de busqueda

--- vilmix-backend/Server/src/routes/authroutes.js
+++ vilmix-backend/Server/src/routes/authroutes.js
@@ -19,8 +19,8 @@ router.post('/login', (req, res) => {
 });
 
 router.post('/register', (req, res) => {
-    const sql = 'INSERT INTO login (email, password) VALUES (?, ?)';
-    const values = [req.body.email, req.body.password];
+    const sql = 'INSERT INTO login (email, password, role) VALUES (?, ?, ?)'; // Modificado para incluir el campo de rol
+    const values = [req.body.email, req.body.password, 'usuario']; // Agregado el valor del rol por defecto
     db.query(sql, values, (error, result) => {
         if (error) {
             console.error('Error en la consulta SQL:', error);
@@ -29,5 +29,26 @@ router.post('/register', (req, res) => {
         return res.status(201).json({ message: 'Usuario registrado correctamente' });
     });
 });
+
+const isAdmin = (req, res, next) => {
+    // Aquí puedes agregar la lógica para verificar el rol del usuario
+    // Por ejemplo, si el usuario tiene un campo 'role' en su objeto de sesión
+    // y su valor es 'admin', entonces puedes permitir el acceso a la ruta
+    if (req.user && req.user.role === 'admin') {
+        // Si el usuario es un administrador, permite el acceso a la ruta
+        next();
+    } else {
+        // Si el usuario no es un administrador, devuelve un error de acceso no autorizado
+        return res.status(403).json({ error: 'Acceso no autorizado' });
+    }
+};
+
+// Ruta protegida que solo puede ser accedida por administradores
+router.get('/admin-only', isAdmin, (req, res) => {
+    // Si se llega a esta parte del código, significa que el usuario es un administrador
+    // y tiene acceso a esta ruta
+    res.json({ message: 'Esta es una ruta solo para administradores' });
+});
+
 
 export default router;
