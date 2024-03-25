6aa31753dc2be3318fbcc94129209df2f3d46291
Belen <arenabelu@gmail.com>
2024-03-25 00:31:46 -0300

    se agrego el filtro de categorias

--- vilmix-backend/Server/src/mailer.js
+++ vilmix-backend/Server/src/mailer.js
@@ -1,29 +1,46 @@
-import nodeMailer from 'nodemailer'
-const transporter = nodeMailer.createTransport({
-    host: "smtp.gmail.com",
-    port: 465,
-    secure: true,
-    auth: {
-        user: "yourEmail@email.com",
-        pass: "yourPassword"
-    }
-})
+const nodemailer = require('nodemailer');
+const uuid = require('uuid');
+const db = require('./db/db');
 
-let mail = {
-    from: "yourEmail@email.com",
-    to: "example@mail.com",
-    subject: "Subject",
-    text: "Text",
-    html: `
-        <h5>Hi, this is a new Email from Nodemailer.</h5>
-    `
-}
+// Configuración de nodemailer (configura tus propias credenciales)
+const transporter = nodemailer.createTransport({
+  service: 'Gmail',
+  auth: {
+    user: 'arenabelu@gmail.com',
+    pass: 'tu_contraseña'
+  }
+});
 
-transporter.sendMail(mail, (error, info) => {
-    if(error) {
-        console.error("Error sending email: ", error);
-    }//end if
-    else {
-        console.log("Email sent.");
-    }//end else
-})
+// Procesar el checkout
+exports.processCheckout = async (req, res) => {
+  const order = req.body;
+
+  try {
+    // Generar número de orden único
+    const orderNumber = uuid.v4();
+
+    // Guardar el pedido en la base de datos
+    const sql = 'INSERT INTO orders (order_number, items, total) VALUES (?, ?, ?)';
+    db.query(sql, [orderNumber, JSON.stringify(order.items), order.total], async (err, result) => {
+      if (err) {
+        console.error('Error al guardar el pedido:', err);
+        res.status(500).json({ message: 'Error al procesar el pedido' });
+        return;
+      }
+
+      // Enviar correo electrónico al cliente
+      const mailOptions = {
+        from: 'tu_correo@gmail.com',
+        to: 'correo_cliente@example.com',
+        subject: 'Tenes un nuevo pedido',
+        text: `Numero de orden :${orderNumber}.`
+      };
+
+      await transporter.sendMail(mailOptions);
+      res.json({ orderNumber: orderNumber });
+    });
+  } catch (error) {
+    console.error('Error en el proceso de checkout:', error);
+    res.status(500).json({ message: 'Error en el proceso de checkout' });
+  }
+};
--- vilmix-backend/Server/src/routes/products.js
+++ vilmix-backend/Server/src/routes/products.js
@@ -36,6 +36,56 @@ router.get('/productos', (req, res) => {
     });
 });
 
+router.delete('/productos/:id', (req, res) => {
+  const productId = req.params.id;
+
+  const sql = 'DELETE FROM products WHERE id = ?';
+  const values = [productId];
+
+  db.query(sql, values, (error, results) => {
+    if (error) {
+      console.error('Error al eliminar el producto:', error);
+      res.status(500).json({ error: 'Error al eliminar el producto' });
+      return;
+    }
+
+    if (results.affectedRows === 0) {
+      // Si no se encontró ningún producto con el ID proporcionado
+      res.status(404).json({ error: 'Producto no encontrado' });
+      return;
+    }
+
+    console.log('Producto eliminado correctamente:', productId);
+    res.status(200).json({ message: 'Producto eliminado correctamente' });
+  });
+});
+
+router.put('/productos/:id', (req, res) => {
+  const productId = req.params.id;
+  const { nombre, imagen, cantidad, descripcion, precio, category } = req.body;
+
+  const sql = 'UPDATE products SET nombre = ?, imagen = ?, cantidad = ?, descripcion = ?, precio = ?, category = ? WHERE id = ?';
+  const values = [nombre, imagen, cantidad, descripcion, precio, category, productId];
+
+  db.query(sql, values, (error, results) => {
+    if (error) {
+      console.error('Error al editar el producto:', error);
+      res.status(500).json({ error: 'Error al editar el producto' });
+      return;
+    }
+
+    if (results.affectedRows === 0) {
+      res.status(404).json({ error: 'Producto no encontrado' });
+      return;
+    }
+
+    console.log('Producto editado correctamente:', productId);
+    res.status(200).json({ message: 'Producto editado correctamente' });
+  });
+});
+
+
+
 router.post('/productos', (req, res) => {
   const { nombre, imagen, cantidad, descripcion, precio, category } = req.body; // Recibir los datos del nuevo producto desde el cuerpo de la solicitud
 
@@ -55,4 +105,17 @@ router.post('/productos', (req, res) => {
   });
 });
 
+router.get('/productos/categoria/:categoria', (req, res) => {
+  const categoria = req.params.categoria;
+  const sql = 'SELECT * FROM products WHERE category = ?';
+  db.query(sql, [categoria], (error, results, fields) => {
+    if (error) {
+      console.error('Error al obtener productos por categoría:', error);
+      res.status(500).json({ error: 'Error al obtener productos por categoría' });
+      return;
+    }
+    console.log(`Productos de la categoría "${categoria}" recibidos del frontend:`, results);
+    res.json(results);
+  });
+});
 export default router;
